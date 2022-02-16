27259e2cac71f342b11e14eda54f4cb2921583e0
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-02-16 14:01:03 +0000

    Update code to load EPD data to the new DB

--- R.ignore/load_epd_data_to_db.R
+++ R.ignore/load_epd_data_to_db.R
@@ -2,6 +2,7 @@
 conn <- dabr::open_conn_mysql("SPECIAL-EPD",
                               password = rstudioapi::askForPassword())
 
+data("EPD_METADATA")
 # Metadata ----
 meta_res <- seq_len(nrow(EPD_METADATA)) %>%
   purrr::map(function(i) {
@@ -20,7 +21,7 @@ meta_res <- seq_len(nrow(EPD_METADATA)) %>%
       # rpd:::update_records(conn = conn, table = "entity", PK = 1:2)
       rpd:::add_records(conn = conn, table = "entity")
   })
-meta_res %>% purrr::flatten_lgl() %>% all()
+meta_res %>% purrr::flatten_lgl() %>% sum()
 
 ## Validate metadata ----
 EPD_METADATA_DB <- dabr::select_all(conn, "entity") %>%
@@ -49,7 +50,7 @@ waldo::compare(EPD_METADATA %>%
                                site_type),
                EPD_METADATA_DB)
 
-## Neotoma ----
+## External reference data ----
 meta_neo_res <- seq_len(nrow(EPD_METADATA)) %>%
   purrr::map(function(i) {
     EPD_METADATA[i, ] %>%
@@ -62,26 +63,27 @@ meta_neo_res <- seq_len(nrow(EPD_METADATA)) %>%
       dplyr::mutate(external_source = "NEOTOMA") %>%
       rpd:::add_records(conn = conn, table = "external_link")
   })
-meta_neo_res %>% purrr::flatten_lgl() %>% all()
+meta_neo_res %>% purrr::flatten_lgl() %>% sum()
 
-### Validate Neotoma metadata ----
-EPD_METADATA_NEO_DB <- dabr::select_all(conn, "neotoma") %>%
+### Validate external reference data ----
+EPD_METADATA_NEO_DB <- dabr::select_all(conn, "external_link") %>%
+  dplyr::filter(external_source == "NEOTOMA") %>%
   dplyr::select(ID_SITE,
                 ID_ENTITY,
-                neotoma_ID_SITE,
-                neotoma_ID_ENTITY,
-                neotoma_site_name,
-                neotoma_entity_name)
+                external_ID_SITE,
+                external_ID_ENTITY,
+                external_site_name,
+                external_entity_name)
 waldo::compare(EPD_METADATA %>%
                  dplyr::select(ID_SITE,
                                ID_ENTITY,
-                               neotoma_ID_SITE = site_id,
-                               neotoma_ID_ENTITY = dataset_id,
-                               neotoma_site_name = site_name,
-                               neotoma_entity_name = entity_name),
+                               external_ID_SITE = site_id,
+                               external_ID_ENTITY = dataset_id,
+                               external_site_name = site_name,
+                               external_entity_name = entity_name) %>%
+                 dplyr::mutate(external_ID_SITE = as.integer(external_ID_SITE),
+                               external_ID_ENTITY = as.integer(external_ID_ENTITY)),
                EPD_METADATA_NEO_DB)
 
 # Dates ----
-EPD_METADATA
-EPD_DATES %>%
-  dplyr::select()
+
