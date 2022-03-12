bfa9dcfbbb225383bc8b8e3f36edc956f2c7e61e
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-03-12 10:06:16 +0000

    Add filter to remove taxa marked as excluded

--- data-raw/EPD_taxa_amalgamation.R
+++ data-raw/EPD_taxa_amalgamation.R
@@ -4,6 +4,10 @@ EPD_taxa_amalgamation <-
   readxl::read_excel("inst/extdata/epd_taxa_list_2022-02-26_SPH.xlsx",
                      skip = 1) %>%
   janitor::clean_names() %>%
+  dplyr::filter(clean_name %>%
+                  stringr::str_to_lower() %>%
+                  stringr::str_squish() %>%
+                  stringr::str_detect("exclude", negate = TRUE)) %>%
   dplyr::mutate(epd_taxa = epd_taxa %>%
                   stringr::str_replace_all(" type", "-type"),
                 clean_name = clean_name %>%
