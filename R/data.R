08ac0a9fddf518d49b3738e07863d597f5476f34
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-03-27 12:08:27 +0100

    Remove unused datasets

--- R/data.R
+++ R/data.R
@@ -1,49 +1,45 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' \code{EPD_COUNTS}
-"EPD_COUNTS"
-
-#' \code{EPD_DATES_coretops}
-"EPD_DATES_coretops"
-
-#' \code{EPD_DATES}
-"EPD_DATES"
-
-#' \code{EPD_METADATA}
-"EPD_METADATA"
-
-#' \code{EPD_taxa_amalgamation}
-"EPD_taxa_amalgamation"
-
-#' \code{EPD_taxa_to_exclude}
-"EPD_taxa_to_exclude"
-
-#' \code{IBERIA_pollen_dates_v2}
-"IBERIA_pollen_dates_v2"
-
-#' \code{IBERIA_pollen_dates_v3}
-"IBERIA_pollen_dates_v3"
-
-#' \code{IBERIA_pollen_v2}
-"IBERIA_pollen_v2"
-
-#' \code{IBERIA_pollen_v3}
-"IBERIA_pollen_v3"
-
 #' \code{age_model}
 "age_model"
+
 #' \code{date_info}
 "date_info"
+
 #' \code{entity}
 "entity"
+
 #' \code{external_link}
 "external_link"
+
 #' \code{model_name}
 "model_name"
+
 #' \code{pollen_count}
 "pollen_count"
+
+#' \code{sample} table
+#'
+#' Contains metadata for each sample record.
+#' @format A data frame (\code{tibble} object) with
+#'    `r nrow(special.epd::sample)` rows and
+#'    `r ncol(special.epd::sample)` variables:
+#' \describe{
+#'   \item{ID_ENTITY}{\code{Unsigned integer}: Unique identifier for the entity (as in \code{entity} table)}
+#'   \item{ID_SAMPLE}{\code{Unsigned integer}: Unique identifier for each sample}
+#'   \item{depth}{\code{Text}: Average sampling depth, in centimetres}
+#'   \item{thickness}{\code{Text}: Sample thickness, in centimetres}
+#'   \item{chronology_name}{\code{Text}: Name assigned to the chronology}
+#'   \item{age_type}{\code{Text}: Type of dating for the sample}
+#'   \item{age}{\code{Integer}: Value dated for the sample}
+#'   \item{age_younger}{\code{Integer}: Lower bound for the sample's age}
+#'   \item{age_older}{\code{Integer}: Upper bound for the sample's age}
+#'   \item{count_type}{\code{Text}: Type of count for the sample}
+#'   \item{sample_type}{\code{Text}: Sample type}
+#' }
 #' \code{sample}
 "sample"
+
 #' \code{taxon_name}
 "taxon_name"
